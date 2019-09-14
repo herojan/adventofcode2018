6e9330f7546c645b30727bbbe9788c16b612b82d
herojan <herojan@users.noreply.github.com>
2019-09-14 13:47:20 +0100

    day 2, part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -9,6 +9,7 @@ fn main() -> Result<()> {
     File::open("input.txt")?.read_to_string(&mut input)?;
 
     part1(&input)?;
+    part2(&input)?;
     Ok(())
 }
 
@@ -34,4 +35,46 @@ fn part1(input: &str) -> Result<()> {
     println!("{}", twice * thrice);
 
     Ok(())
+}
+
+fn part2(input: &str) -> Result<()> {
+    let ids: Vec<&str> = input.lines().collect();
+
+    for i in 0..ids.len() {
+        for j in i + 1..ids.len() {
+            if let Some(common) = common_characters(&ids[i], &ids[j]) {
+                println!("{}", common)
+            }
+        }
+    }
+    Ok(())
+}
+
+fn common_characters(str1: &str, str2: &str) -> Option<String> {
+    if str1.len() != str2.len() {
+        return None;
+    }
+
+    let mut found_diff = false;
+
+    for (c1, c2) in str1.chars().zip(str2.chars()) {
+        if c1 != c2 {
+            if found_diff {
+                return None;
+            }
+
+            found_diff = true;
+        }
+    }
+
+    if found_diff {
+        return Some(
+            str1.chars().zip(str2.chars())
+                .filter(|&(c1, c2)| c1 == c2)
+                .map(|(c, _)| c)
+                .collect()
+        );
+    } else {
+        return None;
+    }
 }
