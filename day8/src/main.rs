678d31347ee5f9fd84782a0ab054b3062f876ea6
herojan <herojan@users.noreply.github.com>
2019-10-11 22:29:22 +0100

    day 8 setup

---
+++ day8/src/main.rs
@@ -0,0 +1,20 @@
+use std::io::Read;
+use std::fs::File;
+
+type Result<T> = ::std::result::Result<T, Box<dyn ::std::error::Error>>;
+
+fn main() -> Result<()> {
+    let mut input = String::new();
+    File::open("input.txt")?.read_to_string(&mut input)?;
+
+    part1(&input)?;
+    Ok(())
+}
+
+fn part1(input: &str) -> Result<()> {
+    for line in input.lines() {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
